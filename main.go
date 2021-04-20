95a82a5c8d2bf56ec7f233a40db1e9c925d44280
trent <trent.hilliam@vorteil.io>
2021-04-20 10:05:11 +1000

    Added ability to replace index.html values with environment variables for ease of dockerization

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"strings"
 	"time"
 )
 
@@ -26,7 +28,13 @@ func AssetHandler(prefix, root string) http.Handler {
 
 		f, err := Assets.Open(assetPath)
 		if os.IsNotExist(err) {
-			return Assets.Open("build/index.html")
+			// if asset does not exist open index.html
+			return os.Open("./index.html")
+		}
+
+		// check if index.html is needed redirect to different file?
+		if strings.Contains(assetPath, "index.html") {
+			return os.Open("./index.html")
 		}
 
 		return f, err
@@ -35,6 +43,20 @@ func AssetHandler(prefix, root string) http.Handler {
 }
 
 func main() {
+	// read embed index html to replace values with environment variables
+	data, err := Assets.ReadFile("build/index.html")
+	if err != nil {
+		log.Fatal("unable to read index.html")
+	}
+
+	new := strings.ReplaceAll(string(data), "API-URL", os.Getenv("API_URL"))
+	newer := strings.ReplaceAll(new, "KEYCLOAK-URL", os.Getenv("KEYCLOAK_URL"))
+
+	err = ioutil.WriteFile("index.html", []byte(newer), 0600)
+	if err != nil {
+		log.Fatal("unable to write new index html file")
+	}
+
 	handler := AssetHandler("/", "build")
 
 	srv := &http.Server{
