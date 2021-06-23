6f4cda56a770134864100bc997192644eae7bae7
trent <trent.hilliam@vorteil.io>
2021-06-23 14:53:36 +1000

    updated react js embed

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"embed"
 	"fmt"
+	"io"
 	"io/fs"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -24,6 +25,60 @@ func (f fsFunc) Open(name string) (fs.File, error) {
 	return f(name)
 }
 
+type IndexFile struct {
+	io.ReadCloser
+	Contents []byte
+}
+
+type IndexFileInfo struct {
+	name    string
+	size    int64
+	mode    fs.FileMode
+	modtime time.Time
+	isDir   bool
+
+	sys interface{}
+}
+
+func (i *IndexFileInfo) Name() string {
+	return i.name
+}
+
+func (i *IndexFileInfo) Size() int64 {
+	return i.size
+}
+
+func (i *IndexFileInfo) ModTime() time.Time {
+	return i.modtime
+}
+
+func (i *IndexFileInfo) IsDir() bool {
+	return i.isDir
+}
+
+func (i *IndexFileInfo) Mode() os.FileMode {
+	return i.mode
+}
+
+func (i *IndexFileInfo) Sys() interface{} {
+	return i.sys
+}
+
+var indexHTML IndexFile
+
+func (index *IndexFile) Stat() (fs.FileInfo, error) {
+	fi := &IndexFileInfo{
+		name:    "index.html",
+		size:    int64(len(index.Contents)),
+		isDir:   false,
+		modtime: time.Now(),
+		mode:    os.ModePerm,
+		sys:     nil,
+	}
+
+	return fi, nil
+}
+
 func AssetHandler(prefix, root string) http.Handler {
 	handler := fsFunc(func(name string) (fs.File, error) {
 		assetPath := path.Join(root, name)
@@ -31,12 +86,12 @@ func AssetHandler(prefix, root string) http.Handler {
 		f, err := Assets.Open(assetPath)
 		if os.IsNotExist(err) {
 			// if asset does not exist open index.html
-			return os.Open("./index.html")
+			return &indexHTML, nil
 		}
 
-		// check if index.html is needed redirect to different file?
+		// check if index.html is needed redirect to a different file?
 		if strings.Contains(assetPath, "index.html") {
-			return os.Open("./index.html")
+			return &indexHTML, nil
 		}
 
 		return f, err
@@ -65,10 +120,16 @@ func main() {
 	new := strings.ReplaceAll(string(data), "API-URL", apiurl)
 	newer := strings.ReplaceAll(new, "KEYCLOAK-URL", keycloakurl)
 
-	err = ioutil.WriteFile("index.html", []byte(newer), 0600)
-	if err != nil {
-		log.Fatal("unable to write new index html file")
+	indexHTML = IndexFile{
+		Contents: []byte(newer),
 	}
+
+	indexHTML.ReadCloser = io.NopCloser(bytes.NewReader(indexHTML.Contents))
+
+	// err = ioutil.WriteFile("index.html", []byte(newer), 0600)
+	// if err != nil {
+	// 	log.Fatal("unable to write new index html file")
+	// }
 
 	handler := AssetHandler("/", "build")
 
